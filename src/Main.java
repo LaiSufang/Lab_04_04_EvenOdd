70c2058b113cf248353c3bf12eb6900803ed891d
Sufang Lai <laisg@mail.uc.edu>
2024-06-03 19:19:22 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args)
+    {
+        int userInput;
+
+        userInput = 8;
+
+        if (userInput % 2 == 0)
+        {
+            System.out.println(userInput + " is an even number.");
+        }
+        else
+        {
+            System.out.println(userInput + " is an odd number.");
+        }
+    }
+}
